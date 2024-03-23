e5e174ed368f9e4b7fcd6f4aa5f499c1b517bb35
ezgm1220 <2431510525@qq.com>
2024-03-23 15:24:35 +0800

    窗口可以脱离viewport

--- src/mGui.cpp
+++ src/mGui.cpp
@@ -13,6 +13,7 @@ void mGui::init(GLFWwindow* window)
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // 启用键盘控制
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // 启用游戏手柄控制
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // 开启窗口悬停功能
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // 开启UI可以渲染viewport外的功能
 
     // Setup Platform/Renderer backends(后端)
     ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
@@ -35,9 +36,19 @@ void mGui::RenderUI()
 
 void mGui::End()
 {
+    ImGuiIO& io = ImGui::GetIO();
+ 
     //渲染结束时必须执行的函数
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    if(io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)// 当开启该项功能时需要注意的事项
+    {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
 }
 
 void mGui::Shutdown()
